27ee893b86f119dabf99c169918129d0ec8dae8a
Millijava <milenashikor@yahoo.com>
2019-08-29 15:54:41 -0400

    Exercise 501

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String about(){
+        return "about";
+    }
+
+    @RequestMapping("/2")
+    public String index(){
+        return "index";
+    }
+    @RequestMapping("/3")
+    public String contactus(){
+        return "contactus";
+    }
+}
